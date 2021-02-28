bb053b286ba2910704380a4f8a548a72789727b4
iPollox <iPollox@users.noreply.github.com>
2021-02-27 23:30:36 -0500

    Update affinity.ps1
    Removed the need for extra user input

--- affinity.ps1
+++ affinity.ps1
@@ -3,12 +3,10 @@
 # Affinity: Starting from CPU0 to the Max (Right to Left), 1 is Enabled and 0 is Disabled.
 # Use the link below to get the Decimal value for your own affinity (e.g: 0101 gives the decimal value of "5", which allows only CPU0 and CPU 2 to be enabled.
 # 0101 = Cpu 3=0, Cpu 2=1, Cpu 1=0, Cpu 0=1   (CPU0 is the first value on the right side)
-# Use: https://www.mathsisfun.com/binary-decimal-hexadecimal-converter.html for the affinity
 
-$steam = $true
-$path = "C:\Program Files (x86)\Steam\steamapps\common\Black Desert Online\"
+# https://www.mathsisfun.com/binary-decimal-hexadecimal-converter.html
+
 $affinity = 21845
-
 
 
 
@@ -30,25 +28,25 @@ if ((Check-Admin) -eq $false) {
     exit
 }
 
-$arg = @{}
-if($steam) { $arg["ArgumentList"] = "--steam" }
-
 # cd first into folder instead of accesing the file directly
-# otherwise the PALauncher makes you re-download the game again.
-cd ${path}
+# otherwise the PALauncher makes you re-download the game again for some odd-reason.
+$path = Split-Path $script:MyInvocation.MyCommand.Path
+cd $path
+
+# Detect if this is the steam version of BDO
+$arg = @{}
+if(Test-Path -Path ".\steam_api.dll") { $arg["ArgumentList"] = "--steam" }
+
+# Launch BDO's patcher
 $app = Start-Process -NoNewWindow -PassThru -FilePath ".\BlackDesertPatcher32.pae" @arg
 
 # Attempt to get the patcher's process
-# If not found after 5 tries just end the script.
-for($tries = 0; $tries -le 5; $tries++) {
+# If not found after 5 seconds just end the script.
+$time = (Get-Date).AddSeconds(5)
+do {
     $status = Get-Process BlackDesertPatcher32.pae -ErrorAction SilentlyContinue
-    Start-Sleep -Seconds 1
-
     if($status) {
-        # Honestly, only the first PA process needs the affinity,
-        # but might as well do it to all 3 of them just in case.
-        foreach($process in $status) {
-            $process.ProcessorAffinity=$affinity
-        }
+        $status.ProcessorAffinity=$affinity
+        exit
     }
-}
+} while((!$status) -and (Get-Date) -lt $time)
